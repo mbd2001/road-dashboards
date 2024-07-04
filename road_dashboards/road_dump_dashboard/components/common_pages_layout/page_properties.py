95aed79d5a6e1e2dc0137d80e24349c929ae5169
Nadav Shemesh <nadavsh@mobileye.com>
2024-07-04 16:08:13 +0300

    Add docs

--- road_dashboards/road_dump_dashboard/components/common_pages_layout/page_properties.py
+++ road_dashboards/road_dump_dashboard/components/common_pages_layout/page_properties.py
@@ -7,13 +7,13 @@ class PageProperties:
     Defines the properties of a single page
 
     Attributes:
-            order (int):
-            icon (str):
-            path (str):
-            title (str):
-            objs_name (str): optional.
-            main_table (str): optional.
-            meta_data_table (str): optional.
+            order (int): the relative order of the page (0 is first page, 1 is second, and so on)
+            icon (str): the icon of the page in the sidebar
+            path (str): page's path
+            title (str): path's title
+            objs_name (str): optional. the name of the objects that the page represent
+            main_table (str): optional. main table represents by the page (rpw, lb, or so on)
+            meta_data_table (str): optional. the meta data table represents by the page
 
     """
 
