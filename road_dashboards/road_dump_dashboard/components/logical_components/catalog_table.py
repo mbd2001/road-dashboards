ba15ea91cd7a02cdccef930b9c85421619db20eb
Beki Shreiber <beki.shreiber@mobileye.com>
2024-07-09 11:03:28 +0300

    Code review changes: checkboxes and renaming

--- road_dashboards/road_dump_dashboard/components/logical_components/catalog_table.py
+++ road_dashboards/road_dump_dashboard/components/logical_components/catalog_table.py
@@ -8,6 +8,7 @@ from road_database_toolkit.dynamo_db.db_manager import DBManager
 
 from road_dashboards.road_dump_dashboard.components.constants.components_ids import (
     DUMP_CATALOG,
+    COLUMN_SELECTOR,
     LOAD_NETS_DATA_NOTIFICATION,
     TABLES,
     UPDATE_RUNS_BTN,
@@ -17,29 +18,32 @@ from road_dashboards.road_dump_dashboard.components.dashboard_layout.layout_wrap
     card_wrapper,
     loading_wrapper,
 )
-from road_dashboards.road_dump_dashboard.components.logical_components.tables_properties import Tables
 
 dump_db_manager = DBManager(table_name="algoroad_dump_catalog", primary_key="dump_name")
 
 all_columns = ["dump_name", "use_case", "user", "total_frames", "last_change", "hfov", "jira"]  # Define globally
+default_unchecked = ["hfov", "jira"]
+default_checked = [col for col in all_columns if col not in default_unchecked]
 
-def layout():
-    default_unchecked = ["hfov", "jira"]
-    default_checked = [col for col in all_columns if col not in default_unchecked]
+column_names = {
+    "dump_name": "Dataset Name",
+    "use_case": "Use Case",
+    "user": "User",
+    "total_frames": "Total Frames",
+    "last_change": "Last Change",
+    "hfov": "HFOV",
+    "jira": "JIRA"
+}
 
-    catalog_data = pd.DataFrame(dump_db_manager.scan())[all_columns]
-    catalog_data["total_frames"] = catalog_data["total_frames"].apply(lambda x: sum(x.values()))
-    catalog_data_dict = catalog_data.to_dict("records")
-
-    column_selector = dbc.DropdownMenu(
+def get_column_selector():
+    return dbc.DropdownMenu(
         label="Select Columns",
         children=[
             dbc.Checklist(
-                options=[{"label": col, "value": col} for col in all_columns],
+                options=[{"label": column_names[col], "value": col} for col in all_columns],
                 value=default_checked,
                 id='column-selector',
-                inline=True,
-                switch=True,
+                inline=False,
                 className='dropdown-item',
                 inputClassName='me-2'
             )
@@ -49,41 +53,49 @@ def layout():
         style={"position": "absolute", "right": "10px", "top": "10px"}
     )
 
+def get_data_table():
+    catalog_data = pd.DataFrame(dump_db_manager.scan())[all_columns]
+    catalog_data["total_frames"] = catalog_data["total_frames"].apply(lambda x: sum(x.values()))
+    catalog_data_dict = catalog_data.to_dict("records")
+
+    columns = [{"name": column_names[col], "id": col} for col in all_columns if col in default_checked]
+
+    return dash_table.DataTable(
+        id=DUMP_CATALOG,
+        columns=columns,
+        data=catalog_data_dict,
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        sort_by=[{"column_id": "last_change", "direction": "desc"}],
+        row_selectable="multi",
+        selected_rows=[],
+        page_action="native",
+        page_current=0,
+        page_size=20,
+        css=[{"selector": ".show-hide", "rule": "display: none"}],
+        style_cell={"textAlign": "left"},
+        style_header={
+            "background-color": "#4e4e50",
+            "fontWeight": "bold",
+            "color": "white",
+        },
+        style_data={
+            "backgroundColor": "white",
+            "color": "rgb(102, 102, 102)",
+        },
+        style_table={
+            "border": "1px solid rgb(230, 230, 230)",
+        },
+    )
+
+def layout():
     layout = html.Div(
         card_wrapper(
             [
                 dbc.Row(html.H2("Datasets Catalog", className="mb-5")),
-                dbc.Row(dbc.Col(column_selector, width={"size": 12})),
-                dbc.Row(
-                    dash_table.DataTable(
-                        id=DUMP_CATALOG,
-                        columns=[{"name": i, "id": i, "deletable": False, "selectable": True} for i in default_checked],
-                        data=catalog_data_dict,
-                        filter_action="native",
-                        sort_action="native",
-                        sort_mode="multi",
-                        sort_by=[{"column_id": "last_change", "direction": "desc"}],
-                        row_selectable="multi",
-                        selected_rows=[],
-                        page_action="native",
-                        page_current=0,
-                        page_size=20,
-                        css=[{"selector": ".show-hide", "rule": "display: none"}],
-                        style_cell={"textAlign": "left"},
-                        style_header={
-                            "background-color": "#4e4e50",
-                            "fontWeight": "bold",
-                            "color": "white",
-                        },
-                        style_data={
-                            "backgroundColor": "white",
-                            "color": "rgb(102, 102, 102)",
-                        },
-                        style_table={
-                            "border": "1px solid rgb(230, 230, 230)",
-                        },
-                    )
-                ),
+                dbc.Row(dbc.Col(get_column_selector(), width={"size": 12})),
+                dbc.Row(get_data_table()),
                 dbc.Row(
                     dbc.Col(dbc.Button("Choose Datasets to Explore", id=UPDATE_RUNS_BTN, className="bg-primary mt-5")),
                 ),
@@ -95,11 +107,11 @@ def layout():
 
 @callback(
     Output(DUMP_CATALOG, 'columns'),
-    Input('column-selector', 'value')
+    Input(COLUMN_SELECTOR, 'value')
 )
 def update_columns(selected_columns):
     # Filter the original list of columns to maintain the order
-    return [{"name": col, "id": col} for col in all_columns if col in selected_columns]
+    return [{"name": column_names.get(col, col), "id": col} for col in all_columns if col in selected_columns]
 
 @callback(
     Output(TABLES, "data"),
