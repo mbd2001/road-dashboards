74ee2709b5098491ab1edcade046461141cd16e1
Nadav Shemesh <nadavsh@mobileye.com>
2024-11-05 10:30:42 +0200

    Fix md scheme col types

--- road_dashboards/road_dump_dashboard/table_schemes/meta_data.py
+++ road_dashboards/road_dump_dashboard/table_schemes/meta_data.py
@@ -1,21 +1,22 @@
 from pypika import Case
-from road_dump_dashboard.table_schemes.base import Base, Column
+
+from road_dashboards.road_dump_dashboard.table_schemes.base import Base, Column
 
 
 class MetaData(Base):
-    gtem_labels_exist: Column = Column("gtem_labels_exist", bool)
     curve_rad_ahead: Column = Column("curve_rad_ahead", float)
-    mdbi_country: Column = Column("mdbi_country", str)
-    mdbi_road_highway: Column = Column("mdbi_road_highway", str)
-    mdbi_road_country: Column = Column("mdbi_road_country", str)
-    mdbi_road_city: Column = Column("mdbi_road_city", str)
-    mdbi_road_freeway: Column = Column("mdbi_road_freeway", str)
-    rightcolor_yellow: Column = Column("rightcolor_yellow", str)
-    leftcolor_yellow: Column = Column("leftcolor_yellow", str)
-    rightcolor_white: Column = Column("rightcolor_white", str)
-    leftcolor_white: Column = Column("leftcolor_white", str)
-    rightcolor_blue: Column = Column("rightcolor_blue", str)
-    leftcolor_blue: Column = Column("leftcolor_blue", str)
+    gtem_labels_exist: Column = Column("gtem_labels_exist", bool)
+    mdbi_country: Column = Column("mdbi_country", bool)
+    mdbi_road_highway: Column = Column("mdbi_road_highway", bool)
+    mdbi_road_country: Column = Column("mdbi_road_country", bool)
+    mdbi_road_city: Column = Column("mdbi_road_city", bool)
+    mdbi_road_freeway: Column = Column("mdbi_road_freeway", bool)
+    rightcolor_yellow: Column = Column("rightcolor_yellow", bool)
+    leftcolor_yellow: Column = Column("leftcolor_yellow", bool)
+    rightcolor_white: Column = Column("rightcolor_white", bool)
+    leftcolor_white: Column = Column("leftcolor_white", bool)
+    rightcolor_blue: Column = Column("rightcolor_blue", bool)
+    leftcolor_blue: Column = Column("leftcolor_blue", bool)
     is_tv_perfect: Column = Column("is_tv_perfect", bool)
     road_type: Case = (
         Case(alias="road_type")
