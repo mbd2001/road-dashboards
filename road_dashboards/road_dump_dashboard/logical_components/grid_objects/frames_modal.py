5b48e16b812b46c7f80375095cb8d4bb4f3c828c
Nadav Shemesh <nadavsh@mobileye.com>
2024-11-20 10:34:00 +0200

    Allow arbitarary number of datasets display

--- road_dashboards/road_dump_dashboard/logical_components/grid_objects/frames_modal.py
+++ road_dashboards/road_dump_dashboard/logical_components/grid_objects/frames_modal.py
@@ -20,10 +20,7 @@ from road_database_toolkit.dynamo_db.drone_view_images.db_manager import DroneVi
 
 from road_dashboards.road_dump_dashboard.graphical_components.frame_drawer import draw_img, draw_top_view
 from road_dashboards.road_dump_dashboard.logical_components.constants.components_ids import META_DATA
-from road_dashboards.road_dump_dashboard.logical_components.constants.layout_wrappers import (
-    card_wrapper,
-    loading_wrapper,
-)
+from road_dashboards.road_dump_dashboard.logical_components.constants.layout_wrappers import loading_wrapper
 from road_dashboards.road_dump_dashboard.logical_components.constants.query_abstractions import diff_labels_subquery
 from road_dashboards.road_dump_dashboard.logical_components.grid_objects.conf_mat_graph import ConfMatGraph
 from road_dashboards.road_dump_dashboard.logical_components.grid_objects.conf_mat_with_dropdown import (
@@ -35,7 +32,7 @@ from road_dashboards.road_dump_dashboard.table_schemes.custom_functions import d
 
 
 class FramesModal(GridObject):
-    IMG_LIMIT = 25
+    IMG_LIMIT = 50
 
     def __init__(
         self,
@@ -60,56 +57,23 @@ class FramesModal(GridObject):
         self.curr_drawn_column_id = self._generate_id("curr_drawn_column")
         self.curr_drawn_column_filter_id = self._generate_id("curr_drawn_column_filter")
 
-        self.main_img_id = self._generate_id("main_img")
-        self.main_world_id = self._generate_id("main_world")
-
-        self.secondary_img_id = self._generate_id("secondary_img")
-        self.secondary_world_id = self._generate_id("secondary_world")
-
+        self.images_layout_id = self._generate_id("images_layout")
         self.next_btn_id = self._generate_id("next_btn")
         self.prev_btn_id = self._generate_id("prev_btn")
+        self.curr_num_of_drawn_datasets_id = self._generate_id("curr_num_of_drawn_datasets")
 
     def layout(self):
         frames_layout = dbc.Modal(
             [
                 dcc.Store(id=self.curr_img_index_id, data=0),
+                dcc.Store(id=self.curr_num_of_drawn_datasets_id),
                 dcc.Store(id=self.curr_drawn_column_id),
                 dcc.Store(id=self.curr_drawn_column_filter_id, data=dump_object(EmptyCriterion())),
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            loading_wrapper(html.Div(dcc.Graph(config={"displayModeBar": False}), id=self.main_img_id)),
-                            width=10,
-                        ),
-                        dbc.Col(
-                            loading_wrapper(
-                                html.Div(dcc.Graph(config={"displayModeBar": False}), id=self.main_world_id)
-                            ),
-                            width=2,
-                        ),
-                    ],
-                ),
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            loading_wrapper(
-                                html.Div(dcc.Graph(config={"displayModeBar": False}), id=self.secondary_img_id)
-                            ),
-                            width=10,
-                        ),
-                        dbc.Col(
-                            loading_wrapper(
-                                html.Div(dcc.Graph(config={"displayModeBar": False}), id=self.secondary_world_id)
-                            ),
-                            width=2,
-                        ),
-                    ],
-                ),
+                html.Div(id=self.images_layout_id),
                 html.Div(id=self.prev_btn_id, hidden=True),
                 html.Div(id=self.next_btn_id, hidden=True),
             ],
             id=self.component_id,
-            centered=True,
             is_open=False,
             className="mw-100 p-5",
         )
@@ -158,14 +122,10 @@ class FramesModal(GridObject):
                     limit=self.IMG_LIMIT,
                 )
                 data = execute(query)
-                main_img_figs, main_world_figs, secondary_img_figs, secondary_world_figs = (
-                    self.compute_images_from_query_data(data, extra_columns)
-                )
+                images_layout = self.generate_layout_from_query_data(data, extra_columns)
 
-                set_props(self.main_img_id, {"children": main_img_figs})
-                set_props(self.main_world_id, {"children": main_world_figs})
-                set_props(self.secondary_img_id, {"children": secondary_img_figs})
-                set_props(self.secondary_world_id, {"children": secondary_world_figs})
+                set_props(self.images_layout_id, {"children": images_layout})
+                set_props(self.curr_num_of_drawn_datasets_id, {"data": 2})
                 set_props(self.curr_img_index_id, {"data": 0})
                 set_props(self.component_id, {"is_open": True})
                 set_props(self.curr_drawn_column_id, {"data": dump_object(conf_mat.column)})
@@ -175,10 +135,8 @@ class FramesModal(GridObject):
         for dropdown_conf_mat in self.triggering_dropdown_conf_mats:
 
             @callback(
-                Output(self.main_img_id, "children", allow_duplicate=True),
-                Output(self.main_world_id, "children", allow_duplicate=True),
-                Output(self.secondary_img_id, "children", allow_duplicate=True),
-                Output(self.secondary_world_id, "children", allow_duplicate=True),
+                Output(self.images_layout_id, "children", allow_duplicate=True),
+                Output(self.curr_num_of_drawn_datasets_id, "data", allow_duplicate=True),
                 Output(self.curr_img_index_id, "data", allow_duplicate=True),
                 Output(self.component_id, "is_open", allow_duplicate=True),
                 Output(self.curr_drawn_column_id, "data"),
@@ -209,7 +167,7 @@ class FramesModal(GridObject):
                     or not secondary_dump
                     or not dynamic_column
                 ):
-                    return no_update, no_update, no_update, no_update, no_update, no_update, no_update, no_update
+                    return no_update, no_update, no_update, no_update, no_update, no_update
 
                 main_tables: list[Base] = load_object(main_tables)
                 md_tables: list[Base] = load_object(md_tables) if md_tables else None
@@ -228,14 +186,10 @@ class FramesModal(GridObject):
                     limit=self.IMG_LIMIT,
                 )
                 data = execute(query)
-                main_img_figs, main_world_figs, secondary_img_figs, secondary_world_figs = (
-                    self.compute_images_from_query_data(data, extra_columns)
-                )
+                images_layout = self.generate_layout_from_query_data(data, extra_columns)
                 return (
-                    main_img_figs,
-                    main_world_figs,
-                    secondary_img_figs,
-                    secondary_world_figs,
+                    images_layout,
+                    2,
                     0,
                     True,
                     dump_object(dynamic_column),
@@ -243,10 +197,8 @@ class FramesModal(GridObject):
                 )
 
         @callback(
-            Output(self.main_img_id, "children"),
-            Output(self.main_world_id, "children"),
-            Output(self.secondary_img_id, "children"),
-            Output(self.secondary_world_id, "children"),
+            Output(self.images_layout_id, "children"),
+            Output(self.curr_num_of_drawn_datasets_id, "data"),
             Output(self.curr_img_index_id, "data"),
             Output(self.component_id, "is_open"),
             Input(self.page_filters_id, "data"),
@@ -261,7 +213,7 @@ class FramesModal(GridObject):
             filters, main_tables, md_tables, main_dump, secondary_dump, column_to_compare, column_filter
         ):
             if not column_to_compare or not main_tables or not md_tables or not main_dump or not secondary_dump:
-                return no_update, no_update, no_update, no_update, no_update, no_update
+                return no_update, no_update, no_update, no_update
 
             main_tables: list[Base] = load_object(main_tables)
             md_tables: list[Base] = load_object(md_tables) if md_tables else None
@@ -281,10 +233,8 @@ class FramesModal(GridObject):
                 limit=self.IMG_LIMIT,
             )
             data = execute(query)
-            main_img_figs, main_world_figs, secondary_img_figs, secondary_world_figs = (
-                self.compute_images_from_query_data(data, extra_columns)
-            )
-            return main_img_figs, main_world_figs, secondary_img_figs, secondary_world_figs, 0, True
+            images_layout = self.generate_layout_from_query_data(data, extra_columns)
+            return images_layout, 2, 0, True
 
         clientside_callback(
             """
@@ -307,58 +257,55 @@ class FramesModal(GridObject):
         )
 
         @callback(
-            Output(self.main_img_id, "children", allow_duplicate=True),
-            Output(self.main_world_id, "children", allow_duplicate=True),
-            Output(self.secondary_img_id, "children", allow_duplicate=True),
-            Output(self.secondary_world_id, "children", allow_duplicate=True),
+            Output(self.images_layout_id, "children", allow_duplicate=True),
             Output(self.curr_img_index_id, "data", allow_duplicate=True),
             Input(self.curr_img_index_id, "data"),
+            Input(self.curr_num_of_drawn_datasets_id, "data"),
             Input(self.prev_btn_id, "n_clicks"),
             Input(self.next_btn_id, "n_clicks"),
             prevent_initial_call=True,
         )
         def update_frame_graphs(
-            ind,
+            img_ind,
+            curr_num_of_drawn_datasets_id,
             prev_n_clicks,
             next_n_clicks,
         ):
             triggered_id = callback_context.triggered_id
-            if not triggered_id or ind is None:
-                return no_update, no_update, no_update, no_update, no_update
+            if not triggered_id or img_ind is None:
+                return no_update, no_update
 
-            main_img = Patch()
-            main_world = Patch()
-            secondary_img = Patch()
-            secondary_world = Patch()
+            images_layout = Patch()
             if triggered_id != self.curr_img_index_id:
                 n_clicks = callback_context.triggered[0]["value"]
                 if n_clicks == 0:
                     return no_update, no_update, no_update, no_update, no_update
 
-                main_img[ind]["props"]["style"]["display"] = "none"
-                main_world[ind]["props"]["style"]["display"] = "none"
-                secondary_img[ind]["props"]["style"]["display"] = "none"
-                secondary_world[ind]["props"]["style"]["display"] = "none"
+                for i in range(curr_num_of_drawn_datasets_id):
+                    images_layout[i]["props"]["children"][0]["props"]["children"]["props"]["children"][img_ind][
+                        "props"
+                    ]["style"]["display"] = "none"
+                    images_layout[i]["props"]["children"][1]["props"]["children"]["props"]["children"][img_ind][
+                        "props"
+                    ]["style"]["display"] = "none"
 
                 if triggered_id == self.prev_btn_id:
-                    ind = (ind - 1) % self.IMG_LIMIT
+                    img_ind = (img_ind - 1) % self.IMG_LIMIT
                 elif triggered_id == self.next_btn_id:
-                    ind = (ind + 1) % self.IMG_LIMIT
+                    img_ind = (img_ind + 1) % self.IMG_LIMIT
 
-            main_img[ind]["props"]["style"]["display"] = "block"
-            main_world[ind]["props"]["style"]["display"] = "block"
-            secondary_img[ind]["props"]["style"]["display"] = "block"
-            secondary_world[ind]["props"]["style"]["display"] = "block"
-            return (
-                main_img,
-                main_world,
-                secondary_img,
-                secondary_world,
-                ind if triggered_id != self.curr_img_index_id else no_update,
-            )
+            for dataset_ind in range(curr_num_of_drawn_datasets_id):
+                images_layout[dataset_ind]["props"]["children"][0]["props"]["children"]["props"]["children"][img_ind][
+                    "props"
+                ]["style"]["display"] = "block"
+                images_layout[dataset_ind]["props"]["children"][1]["props"]["children"]["props"]["children"][img_ind][
+                    "props"
+                ]["style"]["display"] = "block"
+
+            return images_layout, img_ind if triggered_id != self.curr_img_index_id else no_update
 
     @staticmethod
-    def compute_images_from_query_data(data, extra_columns):
+    def generate_layout_from_query_data(data, extra_columns):
         labels_df = FramesModal.parse_labels_df(data, extra_columns)
 
         indexes_df = labels_df[["clip_name", "grabindex"]].drop_duplicates()
@@ -368,13 +315,14 @@ class FramesModal(GridObject):
         images = DroneViewDBManager.load_multiple_clips_images(clip_names, grab_indexes, data_types)
 
         dumps_df_list = [d for _, d in labels_df.groupby(["dump_name"])]
-        img_figs = [[] for _ in range(len(dumps_df_list))]
-        world_figs = [[] for _ in range(len(dumps_df_list))]
-        for dump_df, img_fig, world_fig in zip(dumps_df_list, img_figs, world_figs):
+        final_layout = []
+        for dump_df in dumps_df_list:
             frames_df_list = [i for _, i in dump_df.groupby(["clip_name", "grabindex"])]
+            img_graphs = []
+            world_graphs = []
             for frames_df in frames_df_list:
                 curr_img = images[frames_df.clip_name.iloc[0]]["data"][frames_df.grabindex.iloc[0]][0][0]
-                img_fig.append(
+                img_graphs.append(
                     draw_img(
                         curr_img,
                         frames_df,
@@ -383,9 +331,14 @@ class FramesModal(GridObject):
                         frames_df.grabindex.iloc[0],
                     )
                 )
-                world_fig.append(draw_top_view(frames_df))
+                world_graphs.append(draw_top_view(frames_df))
 
-        return img_figs[0], world_figs[0], img_figs[1], world_figs[1]
+            dump_row = dbc.Row(
+                [dbc.Col(loading_wrapper(img_graphs), width=10), dbc.Col(loading_wrapper(world_graphs), width=2)]
+            )
+            final_layout.append(dump_row)
+
+        return final_layout
 
     @staticmethod
     def parse_labels_df(labels_df: pd.DataFrame, extra_columns: list[Column]):
