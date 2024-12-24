6fe9498d9c8908334b6c9538673183fd223d1288
Nadav Shemesh <nadavsh@mobileye.com>
2024-12-24 17:49:57 +0200

    Add typing

--- road_dashboards/road_dump_dashboard/graphical_components/countries_map.py
+++ road_dashboards/road_dump_dashboard/graphical_components/countries_map.py
@@ -3,14 +3,16 @@ import os
 import re
 
 import numpy as np
+import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import pycountry
 
 main_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open(f"{main_dir}/assets/custom.geo.json") as f:
     geojson = json.load(f)
 
-COUNTRIES_NORMALIZATION = {
+COUNTRIES_NORMALIZATION: dict[str, str] = {
     "USA": "United States",
     "UAE": "United Arab Emirates",
     "Lichstenstein": "Liechtenstein",
@@ -23,7 +25,9 @@ COUNTRIES_NORMALIZATION = {
 }
 
 
-def generate_world_map(countries_data, locations, color, hover_data=None):
+def generate_world_map(
+    countries_data: pd.DataFrame, locations: str, color: str, hover_data: str | list[str] | None = None
+) -> go.Figure:
     fig = px.choropleth_mapbox(
         countries_data,
         geojson=geojson,
@@ -49,7 +53,7 @@ def generate_world_map(countries_data, locations, color, hover_data=None):
     return fig
 
 
-def normalize_countries_names(name):
+def normalize_countries_names(name: str) -> str:
     normalized_name = re.sub("([A-Z]+)", r" \1", name)
     normalized_name = re.sub("  +", " ", normalized_name)
     normalized_name = normalized_name.strip()
@@ -57,12 +61,12 @@ def normalize_countries_names(name):
     return normalized_name
 
 
-def iso_alpha_from_name(name):
+def iso_alpha_from_name(name: str) -> str:
     iso_alpha = pycountry.countries.get(name=name)
     return iso_alpha.alpha_3 if iso_alpha else "Unknown"
 
 
-def normalize_countries_count_to_percentiles(data, num_of_percentiles=10):
+def normalize_countries_count_to_percentiles(data: pd.DataFrame, num_of_percentiles: int = 10) -> np.ndarray:
     N = data.size
     nq = num_of_percentiles
     o = data.argpartition(np.arange(1, nq) * N // nq)
--- road_dashboards/road_dump_dashboard/graphical_components/line_graph.py
+++ road_dashboards/road_dump_dashboard/graphical_components/line_graph.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 def draw_line_graph(
-    data: pd.DataFrame, names: str, values: str, title: str = "", hover: str | None = None, color: str | None = None
-):
+    data: pd.DataFrame,
+    names: str,
+    values: str,
+    title: str = "",
+    hover: str | list[str] | None = None,
+    color: str | None = None,
+) -> go.Figure:
     data = data.sort_values(by=names)
     fig = px.line(data, x=names, y=values, color=color, hover_data=hover, markers=True)
     fig.update_layout(
