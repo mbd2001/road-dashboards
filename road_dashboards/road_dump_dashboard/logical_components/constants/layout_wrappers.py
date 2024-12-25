c755923ecbb6969adab7d9a41bf4f525b5673dee
Nadav Shemesh <nadavsh@mobileye.com>
2024-12-25 14:26:07 +0200

    More typing

--- road_dashboards/road_dump_dashboard/logical_components/constants/layout_wrappers.py
+++ road_dashboards/road_dump_dashboard/logical_components/constants/layout_wrappers.py
@@ -1,12 +1,13 @@
 import dash_bootstrap_components as dbc
 from dash import dcc
+from dash.development.base_component import Component
 
 
-def card_wrapper(object_list, **kwargs):
+def card_wrapper(object_list: list[Component] | Component, **kwargs) -> Component:
     return dbc.Card(dbc.CardBody(object_list), className="mt-5", style={"borderRadius": "15px"}, **kwargs)
 
 
-def loading_wrapper(object_list, **kwargs):
+def loading_wrapper(object_list: list[Component] | Component, **kwargs) -> Component:
     """A Loading component that wraps any other component list and displays a spinner
     until the wrapped component has rendered."""
 
--- road_dashboards/road_dump_dashboard/logical_components/multi_page_objects/dcc_stores.py
+++ road_dashboards/road_dump_dashboard/logical_components/multi_page_objects/dcc_stores.py
@@ -1,7 +1,8 @@
 from dash import dcc, html
+from dash.development.base_component import Component
 
 from road_dashboards.road_dump_dashboard.logical_components.constants.init_data_sources import EXISTING_TABLES
 
 
-def init_dcc_stores():
+def init_dcc_stores() -> Component:
     return html.Div([dcc.Store(id=table, data=None) for table in EXISTING_TABLES])
