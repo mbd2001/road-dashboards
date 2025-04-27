65bfa7455c835d3a2a4db58e92133579dd47e541
Raphael Mamane <raphael.mamane@mobileye.com>
2025-04-27 18:03:41 +0300

    add additional boundaries classes to eval

--- road_dashboards/road_eval_dashboard/pages/pathnet_page/tabs/boundaries_tab.py
+++ road_dashboards/road_eval_dashboard/pages/pathnet_page/tabs/boundaries_tab.py
@@ -39,16 +39,38 @@ from road_dashboards.road_eval_dashboard.utils.consts import BOUNDARY_IGNORE_VAL
 from road_dashboards.road_eval_dashboard.utils.distances import SECONDS
 from road_dashboards.road_eval_dashboard.utils.url_state_utils import create_dropdown_options_list
 
-BOUNDARY_ONTOLOGIES = ["boundaries"]
-ONTOLOGY_CLASSES_NAMES = {"boundaries": BOUNDARY_TYPES_NAMES}
+CATEGORICAL_CLASSES = [
+    ("boundaries", "type"),
+    ("boundaries", "is_re"),
+    ("boundaries", "iscrossable"),
+    ("road_edges", "iselevated"),
+]
+
+
+CLASS_NAMES_MAP = {
+    "boundaries type": BOUNDARY_TYPES_NAMES,
+    "boundaries iscrossable": ["True", "False"],
+    "boundaries is_re": ["True", "False"],
+    "road_edges iselevated": ["True", "False"],
+}
+
+
+def create_bound_name(bound, class_attribute):
+    return f"{bound} {class_attribute}"
+
+
+def parse_bound_name(bound_name):
+    return bound_name.split(" ", 1)
+
+
 EMPTY_FIGURE = {"data": [], "layout": {}}
 
 
-def get_ontology_column_templates(ontology, gt_pred):
+def get_bound_column_templates(bound, class_attribute, gt_pred):
     if gt_pred == "gt" or gt_pred == "label":
-        return f"{ontology}_gt_type"
+        return f"{bound}_gt_{class_attribute}"
     elif gt_pred == "pred" or gt_pred == "prediction":
-        return f"{ontology}_type"
+        return f"{bound}_{class_attribute}"
     else:
         raise ValueError(f"Unknown gt_pred value: {gt_pred}")
 
@@ -100,7 +122,10 @@ boundaries_layout = html.Div(
         get_boundaries_layout(BOUNDARY_DROP_DOWN, PATHNET_BOUNDARY_ACC, "boundaries-acc-threshold-slider"),
         get_boundaries_layout(RE_DROP_DOWN, PATHNET_RE_ACC, "res-acc-threshold-slider"),
     ]
-    + [html.Div(id={"out": "graph", "bound": bo}) for bo in BOUNDARY_ONTOLOGIES]
+    + [
+        html.Div(id={"type": "graph_output_div", "bound": create_bound_name(bound, attr)})
+        for bound, attr in CATEGORICAL_CLASSES
+    ]
 )
 
 
@@ -153,18 +178,20 @@ def update_all_lr_store(nets, meta_data_filters, pathnet_filters, time_value, st
     if not nets:
         return json.dumps({"left": {}, "right": {}, "all": {}})
 
-    bound = store_id["bound"]
+    bound_name = store_id["bound"]  # e.g., "boundaries|type"
+    ontology, class_attribute = parse_bound_name(bound_name)
+    class_names = CLASS_NAMES_MAP[bound_name]
 
     lr_all_data = generate_matrices_graphs_lr(
-        label_col_template=get_ontology_column_templates(bound, "gt"),
-        pred_col_template=get_ontology_column_templates(bound, "pred"),
+        label_col_template=get_bound_column_templates(ontology, class_attribute, "gt"),
+        pred_col_template=get_bound_column_templates(ontology, class_attribute, "pred"),
         nets_tables=nets[PATHNET_BOUNDARIES],
         meta_data_table=nets["meta_data"],
         time_value=f"{time_value:.1f}",
         ignore_val=BOUNDARY_IGNORE_VAL,
         meta_data_filters=meta_data_filters,
         role="",
-        class_names=ONTOLOGY_CLASSES_NAMES[bound],
+        class_names=class_names,
         extra_filters=pathnet_filters,
     )
     return serialize_lr_data(lr_all_data)
@@ -182,18 +209,20 @@ def update_host_lr_store(nets, meta_data_filters, pathnet_filters, time_value, s
     if not nets:
         return json.dumps({"left": {}, "right": {}, "all": {}})
 
-    bound = store_id["bound"]
+    bound_name = store_id["bound"]  # e.g., "boundaries|type"
+    ontology, class_attribute = parse_bound_name(bound_name)
+    class_names = CLASS_NAMES_MAP[bound_name]
 
     lr_all_data = generate_matrices_graphs_lr(
-        label_col_template=get_ontology_column_templates(bound, "gt"),
-        pred_col_template=get_ontology_column_templates(bound, "pred"),
+        label_col_template=get_bound_column_templates(ontology, class_attribute, "gt"),
+        pred_col_template=get_bound_column_templates(ontology, class_attribute, "pred"),
         nets_tables=nets[PATHNET_BOUNDARIES],
         meta_data_table=nets["meta_data"],
         time_value=f"{time_value:.1f}",
         ignore_val=BOUNDARY_IGNORE_VAL,
         meta_data_filters=meta_data_filters,
         role="host",
-        class_names=ONTOLOGY_CLASSES_NAMES[bound],
+        class_names=class_names,
         extra_filters="",
     )
     return serialize_lr_data(lr_all_data)
@@ -209,8 +238,8 @@ def update_host_lr_store(nets, meta_data_filters, pathnet_filters, time_value, s
 def generate_all_dps_figures(side, time_value, lr_all_store_data, graph_id):
     role = ""
     role_str_acc = "all dps"
-    bound = graph_id["bound"]
-    class_names = ONTOLOGY_CLASSES_NAMES[bound]
+    bound_name = graph_id["bound"]
+    class_names = CLASS_NAMES_MAP[bound_name]
 
     if not lr_all_store_data:
         return EMPTY_FIGURE
@@ -228,7 +257,7 @@ def generate_all_dps_figures(side, time_value, lr_all_store_data, graph_id):
         selected_side_data.get(net, {}).get("normalize_mat", default_zeros) for net in net_names
     ]
 
-    mat_title = f"Per-Class Accuracy for {role_str_acc} ({side}, t={time_value:.1f}s)"
+    mat_title = f"Per-Class Accuracy for {side} {bound_name} ({role_str_acc}, t={time_value:.1f}s)"
 
     diagonal_compare_fig = draw_conf_diagonal_compare(
         normalize_mats_for_drawing, net_names, class_names, role=role, mat_name=mat_title
@@ -244,16 +273,16 @@ def generate_all_dps_figures(side, time_value, lr_all_store_data, graph_id):
     Input({"type": BOUNDARIES_HOST_MATRICES_LR_STORE, "bound": MATCH}, "data"),
     State({"type": BOUNDARIES_HOST_DIAG_COMPARE, "bound": MATCH}, "id"),
 )
-def generate_host_figures(side, time_value, lr_all_store_data, graph_id):
+def generate_host_figures(side, time_value, lr_host_store_data, graph_id):
     role = "host"
     role_str_acc = "Host dps"
-    bound = graph_id["bound"]
-    class_names = ONTOLOGY_CLASSES_NAMES[bound]
+    bound_name = graph_id["bound"]
+    class_names = CLASS_NAMES_MAP[bound_name]
 
-    if not lr_all_store_data:
+    if not lr_host_store_data:
         return EMPTY_FIGURE
 
-    lr_all_data = deserialize_lr_data(lr_all_store_data)
+    lr_all_data = deserialize_lr_data(lr_host_store_data)
     net_names = list(lr_all_data.get("all", {}).keys())
     if side not in lr_all_data:
         return EMPTY_FIGURE
@@ -265,7 +294,7 @@ def generate_host_figures(side, time_value, lr_all_store_data, graph_id):
         selected_side_data.get(net, {}).get("normalize_mat", default_zeros) for net in net_names
     ]
 
-    mat_title = f"Per-Class Accuracy for {role_str_acc} ({side}, t={time_value:.1f}s)"
+    mat_title = f"Per-Class Accuracy for {side} {bound_name} ({role_str_acc}, t={time_value:.1f}s)"
 
     diagonal_compare_fig = draw_conf_diagonal_compare(
         normalize_mats_for_drawing, net_names, class_names, role=role, mat_name=mat_title
@@ -291,8 +320,8 @@ def draw_conf_mat(net1_name, net2_name, side, time_value, all_lr_store_data, hos
     if not all_lr_store_data or not host_lr_store_data:
         return EMPTY_FIGURE, EMPTY_FIGURE, EMPTY_FIGURE, EMPTY_FIGURE
 
-    bound = graph_ids[0]["bound"]
-    class_names = ONTOLOGY_CLASSES_NAMES[bound]
+    bound_name = graph_ids[0]["bound"]
+    class_names = CLASS_NAMES_MAP[bound_name]
 
     empty_fig = EMPTY_FIGURE
     default_zeros = np.zeros((len(class_names), len(class_names)))
@@ -319,7 +348,7 @@ def draw_conf_mat(net1_name, net2_name, side, time_value, all_lr_store_data, hos
 
     role_all = ""
     role_str_conf_all = "all dps"
-    conf_mat_title_all = f"Confusion Matrix for {role_str_conf_all} ({side}, t={time_value:.1f}s)"
+    conf_mat_title_all = f"Confusion Matrix for {side} {bound_name} ({role_str_conf_all}, t={time_value:.1f}s)"
 
     conf_all_1, norm_all_1 = get_matrix_data(all_lr_store_data, side, net1_name)
     conf_all_2, norm_all_2 = get_matrix_data(all_lr_store_data, side, net2_name)
@@ -351,7 +380,7 @@ def draw_conf_mat(net1_name, net2_name, side, time_value, all_lr_store_data, hos
 
     role_host = "host"
     role_str_conf_host = "Host dps"
-    conf_mat_title_host = f"Confusion Matrix for {role_str_conf_host} ({side}, t={time_value:.1f}s)"
+    conf_mat_title_host = f"Confusion Matrix for {side} {bound_name} ({role_str_conf_host}, t={time_value:.1f}s)"
 
     conf_host_1, norm_host_1 = get_matrix_data(host_lr_store_data, side, net1_name)
     conf_host_2, norm_host_2 = get_matrix_data(host_lr_store_data, side, net2_name)
@@ -385,26 +414,27 @@ def draw_conf_mat(net1_name, net2_name, side, time_value, all_lr_store_data, hos
 
 
 @callback(
-    Output({"out": "graph", "bound": MATCH}, "children"),
+    Output({"type": "graph_output_div", "bound": MATCH}, "children"),
     Input(NETS, "data"),
-    State({"out": "graph", "bound": MATCH}, "id"),
+    State({"type": "graph_output_div", "bound": MATCH}, "id"),
 )
 def generate_boundaries_layout(nets, graph_id):
+    """Generates the confusion matrix layout for a specific categorical class."""
     if not nets:
         return []
 
-    bound = graph_id["bound"]
+    bound_name = graph_id["bound"]
 
-    all_dps_diag_compare_id = {"type": BOUNDARIES_ALL_DIAG_COMPARE, "bound": bound}
-    host_diag_compare_id = {"type": BOUNDARIES_HOST_DIAG_COMPARE, "bound": bound}
-    conf_mat_option_id_1 = {"type": "net_options", "bound": bound, "index": 1}
-    conf_mat_option_id_2 = {"type": "net_options", "bound": bound, "index": 2}
-    all_dps_conf_mat_id_1 = {"type": BOUNDARIES_ALL_CONF_MATS, "bound": bound, "index": 0}
-    all_dps_conf_mat_id_2 = {"type": BOUNDARIES_ALL_CONF_MATS, "bound": bound, "index": 1}
-    host_conf_mat_id_1 = {"type": BOUNDARIES_HOST_CONF_MATS, "bound": bound, "index": 0}
-    host_conf_mat_id_2 = {"type": BOUNDARIES_HOST_CONF_MATS, "bound": bound, "index": 1}
-    all_lr_store_id = {"type": BOUNDARIES_ALL_MATRICES_LR_STORE, "bound": bound}
-    host_lr_store_id = {"type": BOUNDARIES_HOST_MATRICES_LR_STORE, "bound": bound}
+    all_dps_diag_compare_id = {"type": BOUNDARIES_ALL_DIAG_COMPARE, "bound": bound_name}
+    host_diag_compare_id = {"type": BOUNDARIES_HOST_DIAG_COMPARE, "bound": bound_name}
+    conf_mat_option_id_1 = {"type": "net_options", "bound": bound_name, "index": 1}
+    conf_mat_option_id_2 = {"type": "net_options", "bound": bound_name, "index": 2}
+    all_dps_conf_mat_id_1 = {"type": BOUNDARIES_ALL_CONF_MATS, "bound": bound_name, "index": 0}
+    all_dps_conf_mat_id_2 = {"type": BOUNDARIES_ALL_CONF_MATS, "bound": bound_name, "index": 1}
+    host_conf_mat_id_1 = {"type": BOUNDARIES_HOST_CONF_MATS, "bound": bound_name, "index": 0}
+    host_conf_mat_id_2 = {"type": BOUNDARIES_HOST_CONF_MATS, "bound": bound_name, "index": 1}
+    all_lr_store_id = {"type": BOUNDARIES_ALL_MATRICES_LR_STORE, "bound": bound_name}
+    host_lr_store_id = {"type": BOUNDARIES_HOST_MATRICES_LR_STORE, "bound": bound_name}
 
     diag_compare_graphs_layout = card_wrapper(
         [
@@ -412,14 +442,14 @@ def generate_boundaries_layout(nets, graph_id):
                 [
                     dbc.Col(
                         [
-                            html.H6(f"All DP Per-Class Accuracy for {bound}", style={"textAlign": "center"}),
+                            html.H6(f"All DP Per-Class Accuracy for {bound_name}", style={"textAlign": "center"}),
                             loading_wrapper(graph_wrapper(all_dps_diag_compare_id)),
                         ],
                         width=6,
                     ),
                     dbc.Col(
                         [
-                            html.H6(f"Host DP Per-Class Accuracy for {bound}", style={"textAlign": "center"}),
+                            html.H6(f"Host DP Per-Class Accuracy for {bound_name}", style={"textAlign": "center"}),
                             loading_wrapper(graph_wrapper(host_diag_compare_id)),
                         ],
                         width=6,
@@ -431,7 +461,7 @@ def generate_boundaries_layout(nets, graph_id):
 
     controls_layout = card_wrapper(
         [
-            dbc.Row([html.H4(f"Confusion Matrix Comparison for {bound}", style={"textAlign": "center"})]),
+            dbc.Row([html.H4(f"Confusion Matrix Comparison for {bound_name}", style={"textAlign": "center"})]),
             dcc.Store(id=all_lr_store_id),
             dcc.Store(id=host_lr_store_id),
             dbc.Row(
@@ -456,9 +486,9 @@ def generate_boundaries_layout(nets, graph_id):
                             dcc.Dropdown(
                                 id="boundaries-side-dropdown",
                                 options=[
-                                    {"label": "Both Boundaries", "value": "all"},
-                                    {"label": "Left Boundary", "value": "left"},
-                                    {"label": "Right Boundary", "value": "right"},
+                                    {"label": "Both Sides", "value": "all"},
+                                    {"label": "Left Side", "value": "left"},
+                                    {"label": "Right Side", "value": "right"},
                                 ],
                                 value="all",
                                 clearable=False,
@@ -493,7 +523,7 @@ def generate_boundaries_layout(nets, graph_id):
         [
             dbc.Row(
                 html.H5(
-                    f"All Data Points Confusion Matrices for {bound}",
+                    f"All Data Points Confusion Matrices for {bound_name}",
                     style={"textAlign": "center", "marginTop": "10px"},
                 )
             ),
@@ -509,7 +539,7 @@ def generate_boundaries_layout(nets, graph_id):
             ),
             dbc.Row(
                 html.H5(
-                    f"Host Data Points Confusion Matrices for {bound}",
+                    f"Host Data Points Confusion Matrices for {bound_name}",
                     style={"textAlign": "center", "marginTop": "20px"},
                 )
             ),
